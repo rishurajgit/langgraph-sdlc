88c4955c2febb6cbe8735af17637b109f745d0eb
rishurajgit <rajrishu.cs@gmail.com>
2026-07-15 14:28:42 +0530

    Implemented Pydantic Settings

---
+++ app/config/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    GEMINI_API_KEY: str
+    GEMINI_MODEL: str
+    
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+    
+settings = Settings()
