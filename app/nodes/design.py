6d36991e4f7a2247327df865b18ca788f1ac02f7
rishurajgit <rajrishu.cs@gmail.com>
2026-07-15 17:05:09 +0530

    Improved Design for creating Design Document as per user story

--- app/nodes/design.py
+++ app/nodes/design.py
@@ -1,13 +1,30 @@
 from app.state.state import SDLCState
+from app.services.llm import invoke_llm
 
 def design_node(state: SDLCState):
     
     design = f"""
-    Design Document
-    Based on User Stories
+    You are a Senior Software Architect.
+    Base on the following user stories, generate a Software Design Document.
+    
+    Include:
+    System Overview,
+    Functional Requirements,
+    Non-functional Requirements,
+    High-level Architecture,
+    Suggested Tech Stack,
+    Database Design,
+    API endpoints,
+    Modules
+    
+    User Stories:
     {state["user_stories"]}
+    
+    Return only the Design Document.
     """
-    print(design)
+    
+    design = invoke_llm(design)
+    # print(design)
     return{
         "design_doc": design
     }
